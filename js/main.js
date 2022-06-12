4510676eb00871830ff120e1c9ab1278955a5dd8
irwins7 <irwins1337@gmail.com>
2022-06-13 02:10:37 +0500

    hero done

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const elMenuBtn = document.querySelector(".header__menu-btn");
 const elHeader = document.querySelector(".header");
+const elBody = document.querySelector("body");
 
 elMenuBtn.addEventListener("click", () => {
   elHeader.classList.toggle("header--active");
+  elBody.classList.toggle("hidden");
 });
