624d928cfab16e4e5bf73047510aba4fb06378a5
irwins7 <irwins1337@gmail.com>
2022-06-13 04:00:16 +0500

    featured done

--- js/main.js
+++ js/main.js
@@ -6,3 +6,34 @@ elMenuBtn.addEventListener("click", () => {
   elHeader.classList.toggle("header--active");
   elBody.classList.toggle("hidden");
 });
+
+// Slick
+
+$('.featured__list').slick({
+    dots: true,
+    arrows:false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+          breakpoint: 844,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+      }
+    ]
+  });
